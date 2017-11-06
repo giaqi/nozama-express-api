0d7d97db9d3f03c5bd58a76386b57ab01a6d9e2e
hilarybrown <hilary.brown@libertymutual.com>
2017-11-06 13:01:09 -0500

    Adds index method to Products resource
    Adds index method to products controller
    tested successfully with curl script
    
    Pair Programmers: Hilary Brown and Rachel Jagodowski

--- app/models/product.js
+++ app/models/product.js
@@ -2,19 +2,6 @@
 
 const mongoose = require('mongoose')
 
-// delete after testing
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/nozama-express-api', {
-  useMongoClient: true
-})
-
-const db = mongoose.connection
-
-const done = function () {
-  db.close()
-}
-// end testing
-
 const productSchema = new mongoose.Schema({
   picture_URL: {
     type: String,
@@ -50,18 +37,5 @@ const productSchema = new mongoose.Schema({
 })
 
 const Product = mongoose.model('Product', productSchema)
-
-// testing below here
-Product.create({
-  picture_URL: 'https://imgur.com/QOfuwAs',
-  name: 'Henry 200',
-  description: 'The smiley Henry 200 vacuum cleaner',
-  price: 125
-})
-  .then(result => result.toObject())
-  .then(console.log)
-  // add this .catch second to last so that the db still closes
-  .catch(console.error)
-  .then(done)
 
 module.exports = Product
