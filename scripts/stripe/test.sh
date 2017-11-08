96a7a260cc5d0afd2011275a343ac4ad03d33d9d
jeffkeilman <jkeilman93@gmail.com>
2017-11-08 15:20:12 -0500

    Remove test script

--- scripts/stripe/test.sh
+++
@@ -1,6 +0,0 @@
-curl https://api.stripe.com/v1/charges \
-   -u sk_test_qYebBMofUkr3zAn2p5E6NYvP: \
-   -d amount=1000 \
-   -d currency=usd \
-   -d description="Example charge" \
-   -d source=tok_visa
