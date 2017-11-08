586a28d824eca07e11237940c76f13796d0c07e8
jago425 <rmjagodowski@gmail.com>
2017-11-08 10:25:08 -0500

    Added CRUD for Index and Show
    -curl scripts passed
    -removed set model for update and destroy
    
    Pair Programming: Rachel Jagodowski and Jeff Keilman

--- app/controllers/purchases.js
+++ app/controllers/purchases.js
@@ -49,29 +49,12 @@ const create = (req, res, next) => {
   }
 }
 
-const update = (req, res, next) => {
-  delete req.body.purchase._owner  // disallow owner reassignment.
-
-  req.purchase.update(req.body.purchase)
-    .then(() => res.sendStatus(204))
-    .catch(next)
-}
-
-const destroy = (req, res, next) => {
-  req.purchase.remove()
-    .then(() => res.sendStatus(204))
-    .catch(next)
-}
-
 module.exports = controller({
   index,
   show,
-  create,
-  update,
-  destroy
+  create
 }, { before: [
   { method: setUser, only: ['index', 'show', 'create'] },
   { method: authenticate, except: ['index', 'show'] },
-  { method: setModel(Purchase), only: ['show'] },
-  { method: setModel(Purchase, { forUser: true }), only: ['update', 'destroy'] }
+  { method: setModel(Purchase), only: ['show'] }
 ] })
